10f56b3deff5f9acd259068c69d8cc242ea7bc23
Khaled Chabbou <chabbou85@gmail.com>
2015-03-08 21:16:18 +0000

    Corriger le lien entre les tables categorie et webcam

--- app/models/webcam.rb
+++ app/models/webcam.rb
@@ -1,6 +1,6 @@
 class Webcam < ActiveRecord::Base
   belongs_to :user
+  belongs_to :categorie
   has_many :comments, depend: :destroy
   has_many :user, through: :comments
-  has_many :categories, depend: :destroy
 end
