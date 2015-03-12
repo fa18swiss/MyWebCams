78736913494b2e2779430671ff6bd359e5712568
fa18swiss <fa18swiss@users.noreply.github.com>
2015-03-12 14:07:33 +0000

    Change Seed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150308184347) do
+ActiveRecord::Schema.define(version: 20150308205923) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,7 +51,7 @@ ActiveRecord::Schema.define(version: 20150308184347) do
   add_index "favoris", ["webcam_id"], name: "index_favoris_on_webcam_id", using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "pseudo"
+    t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "email",                  default: "", null: false
