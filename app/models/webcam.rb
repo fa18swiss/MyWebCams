534f2e389d33044c866e6de36f5c7db3da2b7a82
fa18swiss <fa18swiss@users.noreply.github.com>
2015-03-12 16:16:53 +0000

    Change bug in model

--- app/models/webcam.rb
+++ app/models/webcam.rb
@@ -1,6 +1,6 @@
 class Webcam < ActiveRecord::Base
   belongs_to :use
   belongs_to :categorie
-  has_many :comments, depend: :destroy
+  has_many :comments, dependent: :destroy
   has_many :user, through: :comments
 end
