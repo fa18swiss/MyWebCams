97996152706f60b45c37ecf3ad884665cf88818f
Khaled Chabbou <chabbou85@gmail.com>
2015-03-13 14:21:09 +0000

    Modification du fichier favoris.rb

--- db/migrate/20150307162126_create_favoris.rb
+++ db/migrate/20150307162126_create_favoris.rb
@@ -2,8 +2,6 @@ class CreateFavoris < ActiveRecord::Migration
   def change
     create_table :favoris do |t|
       t.string :ordre
-      t.integer :idUser
-      t.integer :idWebCam
       t.references :user, index: true
       t.references :webcam, index: true
 
