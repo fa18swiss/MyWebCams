ba2c1629429afcbedfd7501d8f16327f099163d1
fa18swiss <fa18swiss@users.noreply.github.com>
2015-03-19 18:38:32 +0000

    Bug favoris

--- app/models/webcam.rb
+++ app/models/webcam.rb
@@ -1,9 +1,8 @@
 class Webcam < ActiveRecord::Base
 
-  belongs_to :user, :foreign_key => 'id'
-  belongs_to :categorie, :foreign_key => 'id'
+  belongs_to :user
   has_many :comments, dependent: :destroy
-  has_many :user, through: :comments
+  has_many :favoris, dependent: :destroy
 
   validates :nom,
             length: {minimum: 3, maximum: 50,
