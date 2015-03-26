ec085583f3890bf8101e91b287a70b5e0e58ac6a
Khaled Chabbou <chabbou85@gmail.com>
2015-03-26 10:31:16 +0000

    update schema &  migration

--- db/migrate/20150326111725_add_webcams.rb
+++ db/migrate/20150326111725_add_webcams.rb
@@ -1,10 +1,10 @@
 class AddWebcams < ActiveRecord::Migration
   def up
     Webcam.create([
-      {nom: "LSGC east", url:"http://www.leseplaturesairport.ch/site/images/stories/webcam/LSGC1.jpg", latitude:47.084820, longitude:6.796219, orientation:60},
-      {nom: "LSGC west", url:"http://www.leseplaturesairport.ch/site/images/stories/webcam/LSGC3.jpg", latitude:47.084820, longitude:6.796219, orientation:240},
-      {nom: "LSGN east", url:"http://www.neuchatel-airport.ch/webcams/lsgn_east.jpg", latitude:46.959773, longitude:6.864211},
-      {nom: "LSGN west", url:"http://www.neuchatel-airport.ch/webcams/lsgn_west.jpg", latitude:46.959773, longitude:6.864211}
+      {name: "LSGC east", url:"http://www.leseplaturesairport.ch/site/images/stories/webcam/LSGC1.jpg", latitude:47.084820, longitude:6.796219, orientation:60},
+      {name: "LSGC west", url:"http://www.leseplaturesairport.ch/site/images/stories/webcam/LSGC3.jpg", latitude:47.084820, longitude:6.796219, orientation:240},
+      {name: "LSGN east", url:"http://www.neuchatel-airport.ch/webcams/lsgn_east.jpg", latitude:46.959773, longitude:6.864211},
+      {name: "LSGN west", url:"http://www.neuchatel-airport.ch/webcams/lsgn_west.jpg", latitude:46.959773, longitude:6.864211}
     ])
   end
 end
