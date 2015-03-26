57cc10e066c5a33988c5ba2bcceae027d26baf87
Kilian Brandt <brandtdg.kilian@gmail.com>
2015-03-26 14:17:37 +0000

    Added username in user table and login now available with both email or username case insensitive

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150326111725) do
+ActiveRecord::Schema.define(version: 20150326140236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,7 +39,6 @@ ActiveRecord::Schema.define(version: 20150326111725) do
   add_index "favoris", ["webcam_id"], name: "index_favoris_on_webcam_id", using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "pseudo"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "email",                  default: "", null: false
@@ -55,11 +54,13 @@ ActiveRecord::Schema.define(version: 20150326111725) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string   "username"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
   create_table "webcams", force: true do |t|
     t.string   "name"
