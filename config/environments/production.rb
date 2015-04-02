651758a53a5d900d2c47ccf08f49ec91899fe486
fa18swiss <fa18swiss@users.noreply.github.com>
2015-04-02 12:57:43 +0000

    Fix bug in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # http://guides.rubyonrails.org/configuring.html#deploy-to-a-subdirectory-relative-url-root
+  config.action_controller.relative_url_root = '/MyWebCams'
 end
