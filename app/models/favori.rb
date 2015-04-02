0c16b5fca0d684403ea097c0fef8db8f7b684afa
Khaled Chabbou <chabbou85@gmail.com>
2015-04-02 15:18:15 +0000

    Ajout de la suppression en cascade des favoris

--- app/models/favori.rb
+++ app/models/favori.rb
@@ -1,6 +1,6 @@
 class Favori < ActiveRecord::Base
-  belongs_to :user, :foreign_key => 'id'
-  belongs_to :webcam, :foreign_key => 'id'
+  belongs_to :user, :foreign_key => 'id', dependent: :destroy
+  belongs_to :webcam, :foreign_key => 'id', dependent: :destroy
 
   def self.forUserWebcam(user_id, webcam_id)
     return where("user_id = ? AND webcam_id = ?", user_id, webcam_id)
